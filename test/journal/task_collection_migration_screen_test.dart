a5d22f3602332f66db40b9a8c0e85c52282c4a88
Marcelo Trindade <49993899+marcelositr@users.noreply.github.com>
2026-09-03 18:43:23 -0300

    test(journal): update monthly migration fake

--- test/journal/task_collection_migration_screen_test.dart
+++ test/journal/task_collection_migration_screen_test.dart
@@ -194,6 +194,9 @@ final class _MonthlyJournal implements MonthlyJournalDataSource {
       );
 
   @override
+  Future<MonthlyLogSnapshot?> find(String periodStart) => load(periodStart);
+
+  @override
   Future<void> captureCalendarEvent({
     required String logId,
     required String calendarDate,
