d9e51a4c3ec1c4dc8fbfc843ff07e56f67ab1f21
Marcelo Trindade <49993899+marcelositr@users.noreply.github.com>
2026-09-05 19:39:09 -0300

    feat(app): point About to issue chooser

--- lib/app/app_info.dart
+++ lib/app/app_info.dart
@@ -2,7 +2,8 @@ abstract final class DaymarkAppInfo {
   static const String version = '1.0.0-alpha.2+2';
   static const String website = 'https://devnux.com.br/daymark';
   static const String sourceCode = 'https://github.com/marcelositr/daymark';
-  static const String issues = 'https://github.com/marcelositr/daymark/issues';
+  static const String issues =
+      'https://github.com/marcelositr/daymark/issues/new/choose';
   static const String author = 'Marcelo Trindade';
   static const String authorWebsite = 'https://devnux.com.br';
   static const String license = 'GPL-3.0-or-later';
